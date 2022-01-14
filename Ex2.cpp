7abcd5598786b109ec47efc56ec7f0e9eff9a877
foptun <oixuier.toppfun@gmail.com>
2022-01-14 21:46:04 +0700

    Create Ex2.cpp

---
+++ Ex2.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n = 0;
+    int x = 0;
+    
+    printf("Enter size of matrix (n x n) : ");
+    scanf("%d", &n);
+ 
+ 
+    // int **matrix = (int**) malloc(n * sizeof(int*));
+    int matrix[n][n];
+    
+    // printf("%d", matrix[0][0]);
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &(matrix[i][j]) );   
+        }
+    }
+    
+    printf("\nEnter a scalar: ");
+    scanf("%d", &x);
+    
+    printf("\nResult: \n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", (x * matrix[i][j]) );
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
